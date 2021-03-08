2d91767ea6aa3884aa2a9780831732194c447948
Tomasz Bączek <tb054379@student.ath.edu.pl>
2021-03-08 14:27:09 +0100

    Uporządkowanie po scaleniu

--- KOProject/MainWindow.xaml.cs
+++ KOProject/MainWindow.xaml.cs
@@ -15,14 +15,11 @@ using System.Windows.Shapes;
 
 namespace KOProject
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            
         }
     }
 }
