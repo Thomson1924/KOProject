5bf7605a53ab3bf82d6c9d6d856ff4f6a52928bf
Tomasz Bączek <tb054379@student.ath.edu.pl>
2021-03-15 13:45:19 +0100

    Wprowadzenie danych z silnika wyszukiwania

--- KOProject/MainWindow.xaml.cs
+++ KOProject/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
 
 namespace KOProject
 {
@@ -22,6 +23,8 @@ namespace KOProject
     {
         public MainWindow()
         {
+
+            var ListSites = ScrapList($"http://www.nokaut.pl/{typedCategory}/produkt:{typedThing}");
             InitializeComponent();
         }
     }
---
+++ KOProject/SearchEngine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack
+
+namespace KOProject
+{
+    public class SearchEngine
+    {
+        
+
+        public static List<ListSite> ScrapList(string html)
+        {
+            HtmlWeb web = new HtmlWeb();
+            List<ListSite> listSites = new List<ListSite>();
+            var htmlDoc = web.Load(html);
+            try
+            {
+                foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//div/div")
+                .Where(x => x.GetAttributeValue("class", string.Empty)
+                .Equals("product-box"))
+                .ToList())
+                {
+                    ListSite element = new ListSite();
+                    element.Link = link.SelectSingleNode("a").GetAttributeValue("href", string.Empty);
+                    element.Image = link.SelectSingleNode("a/div/img").GetAttributeValue("src", string.Empty);
+                    element.Name = link.SelectSingleNode("a/div/img").GetAttributeValue("alt", string.Empty);
+                    element.Price = link.SelectSingleNode("a/div/div/div/meta").GetAttributeValue("content", string.Empty);
+
+                    listSites.Add(element);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return listSites;
+        }
+    }
+    
+}
